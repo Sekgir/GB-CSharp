0c8078b8edab0405f4e3db909adb0a1050016f38
Sergey <Sekgir@mail.ru>
2021-02-13 17:49:15 +0300

    Добавьте файлы проекта.

---
+++ GB-CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GB_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя пользователя: ");
+            string username = Console.ReadLine();
+            string curDate = DateTime.Now.ToString("d");
+            Console.WriteLine($"Привет, {username}, сегодня {curDate}");
+
+            Console.ReadKey();
+        }
+    }
+}
